f45624c616f7b297c9aaa795c7c0844da9493d42
joaoppn <jppn4430@gmail.com>
2024-01-17 07:43:09 -0300

    add coments

--- TTC_hash.cpp
+++ TTC_hash.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// Definição da classe RTuple
 class RTuple
 {
 public:
@@ -13,33 +14,24 @@ public:
     RTuple() : RTuple(-1, -1, -1, -1){};
     RTuple(int a, int b, int c, int d) : u(a), v(b), Tminus(c), Tplus(d){};
 
-    // empty
-
+    // Método para verificar se a RTuple está vazia
     bool empty()
     {
-        if (Tminus == -1 && Tplus == -1)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return (Tminus == -1 && Tplus == -1);
     }
 
-    // print
+    // Método para imprimir uma RTuple
     void print_tuple()
     {
         cout << "(" << u << ", " << v << ", " << Tminus << ", " << Tplus << ") ";
     }
 
-    // compare
-
+    // Método para verificar se a RTuple contém outra RTuple
     bool contains(RTuple other1, RTuple other2)
     {
         if (other1.empty())
         {
-            return false; //(Tplus >= other2.Tminus);
+            return false;
         }
         else if (other2.empty())
         {
@@ -49,6 +41,7 @@ public:
         return (Tminus >= other1.Tminus && Tplus <= other2.Tplus);
     }
 
+    // Operadores de comparação para ordenação
     bool operator<(const RTuple &other) const
     {
         if (Tminus != other.Tminus)
@@ -61,7 +54,6 @@ public:
         }
     };
 
-    // Operador de comparação para ordenação (<=)
     bool operator<=(const RTuple &other) const
     {
         if (Tminus != other.Tminus)
@@ -76,37 +68,25 @@ public:
 
     bool operator!=(const RTuple &other) const
     {
-        if (Tminus != other.Tminus or Tplus != other.Tplus)
-        {
-            return true;
-        }
-        return false;
+        return (Tminus != other.Tminus || Tplus != other.Tplus);
     }
 
     bool operator==(const RTuple &other) const
     {
-        if (Tminus == other.Tminus or Tplus == other.Tplus)
-        {
-            return true;
-        }
-        return false;
+        return (Tminus == other.Tminus || Tplus == other.Tplus);
     }
 };
 
+// Estrutura de comparação para o conjunto de RTuples
 struct SetCompare
 {
-
     bool operator()(RTuple lhs, RTuple rhs) const
     {
-        // cout<<"SETCOMPARE: ";
-        // lhs.print_tuple();
-        // cout <<" ";
-        // rhs.print_tuple();
-        // cout << "\n";
         return lhs < rhs;
     }
 };
 
+// Hashing para pares de inteiros (usado para a tabela hash)
 struct pair_hash
 {
     template <class T1, class T2>
@@ -115,12 +95,12 @@ struct pair_hash
         auto h1 = std::hash<T1>{}(p.first);
         auto h2 = std::hash<T2>{}(p.second);
 
-        // Combine the hash values in a way that depends on your use case.
-        // You can use xor or any other suitable method.
+        // Combina os valores de hash de uma maneira adequada para o caso
         return h1 ^ h2;
     }
 };
 
+// Definição da classe Temporal_Graph
 class Temporal_Graph
 {
 public:
@@ -132,24 +112,24 @@ public:
 
     Temporal_Graph(int a) : delta(1), hashTable({{}}), tam(a){};
 
-    // auxiliar
+    // Métodos auxiliares
 
     int get_node_index(const std::string &node_str) const
     {
+        // Método para obter o índice do nó a partir de sua representação (string ou inteiro)
         if (node_str.length() == 1 && isalpha(node_str[0]))
         {
-            // Se for um único caractere alfabético (como 'a', 'b', etc.)
             return node_str[0] - 'a';
         }
         else
         {
-            // Caso contrário, assume que é um número inteiro
             return std::stoi(node_str);
         }
     }
 
-    set<int> neighboursIn(int a) // pega os vizinhos [x][a] de a
+    set<int> neighboursIn(int a)
     {
+        // Método para obter vizinhos de entrada do nó 'a'
         set<int> nIn{};
         for (int x = 0; x < tam; x++)
         {
@@ -161,8 +141,9 @@ public:
         return nIn;
     }
 
-    set<int> neighboursOut(int a) // pega os vizinhos [a][x] de a
+    set<int> neighboursOut(int a)
     {
+        // Método para obter vizinhos de saída do nó 'a'
         set<int> nOut{};
         for (int x = 0; x < tam; x++)
         {
@@ -176,30 +157,22 @@ public:
 
     void insert(int a, int b, int t1, int t2)
     {
-
+        // Método para inserir uma nova RTuple na tabela hash
         RTuple newTuple(a, b, t1, t2);
-        // cout << "add";
-        // newTuple.print_tuple();
-        // cout << "\n - \n";
 
         if (hashTable[{a, b}].empty())
         {
             set<RTuple, SetCompare> newSet = {newTuple};
             hashTable[make_pair(a, b)] = newSet;
-            
         }
         else
         {
-            // Removendo redundancias
-            // finding the lower_boundary l1
+            // Removendo redundâncias
             auto l1 = find_prev(a, b, t2);
-            // cout<<"l1 bruto: ";
-            // l1.print_tuple();
-            // cout<<"\n";
             if (l1.Tplus < t2)
             {
                 if (l1.Tminus >= t1)
-                { // t1 t2
+                {
                     return;
                 }
                 else
@@ -208,15 +181,12 @@ public:
                 }
             }
 
-            // finding upperboundaty l2
             auto l2 = find_next(a, b, t1);
 
-            if (!l2.empty() and l2.Tminus > t1)
+            if (!l2.empty() && l2.Tminus > t1)
             {
                 l2 = find_prev(a, b, l2.Tplus);
             }
-
-            // verifica se l1 e l2 são valores aceitaveis
 
             set<RTuple, SetCompare>::iterator it1, it2;
 
@@ -238,35 +208,19 @@ public:
                 it2 = hashTable[{a, b}].find(l2);
             }
 
-            // cout << "Set[" << a << "][" << b << "]: ";
-            //  for (auto tupla : hashTable[{a, b}])
-            //  {
-            //      tupla.print_tuple();
-            //  }
-            //  cout << "\n Retirando de ";
-            //  l1.print_tuple();
-            //  cout << " ate ";
-            //  l2.print_tuple();
-
-            // conjunto temporário para armazenar elementos a serem removidos
             set<RTuple, SetCompare> elementsToRemove;
 
             while (it1 != it2)
             {
-
-                // armazena o elemento em elementsToRemove em vez de removê-lo diretamente
                 auto elem = *it1;
                 if (elem.contains(l1, l2))
                 {
-                    // cout << "Retirado: ";
-                    elem.print_tuple();
                     elementsToRemove.insert(elem);
                 }
 
                 ++it1;
             }
 
-            // agora, remova os elementos do conjunto
             for (auto elem : elementsToRemove)
             {
                 hashTable[{a, b}].erase(elem);
@@ -277,6 +231,7 @@ public:
 
     RTuple find_next(int u, int v, int t)
     {
+        // Método para encontrar a próxima RTuple a partir de um tempo 't'
         set<RTuple, SetCompare> tuplas = hashTable[{u, v}];
         RTuple next(u, v, t, -1);
         auto it = tuplas.lower_bound(next);
@@ -289,25 +244,11 @@ public:
         {
             return *it;
         }
-        // set<RTuple, SetCompare> tuplas = hashTable[{u, v}];
-        // RTuple early(u, v, -1, -1);
-
-        // for (const auto &tupla : tuplas)
-        // {
-        //     if (tupla.Tminus >= t);
-        //     {
-        //         cout<<"vdd";
-        //         early = tupla;
-        //         return early;
-        //     }
-
-        // }
-        // return early;
     }
 
     RTuple find_prev(int u, int v, int t)
     {
-
+        // Método para encontrar a RTuple mais recente antes de um tempo 't'
         set<RTuple, SetCompare> tuplas = hashTable[{u, v}];
         RTuple latest(u, v, -1, -1);
 
@@ -321,16 +262,19 @@ public:
         return latest;
     }
 
-    // main functions
+    // Funções principais
+
     void add(string u_str, string v_str, int t)
     {
+        // Método para adicionar um novo contato entre 'u' e 'v' no tempo 't'
         int a = get_node_index(u_str);
         int b = get_node_index(v_str);
         insert(a, b, t, t + delta);
         set<pair<int, int>> aux;
         set<int> neigh_a = neighboursIn(a);
         set<int> neigh_b = neighboursOut(b);
-        // neighbours in
+
+        // Vizinhos de entrada
         for (auto x : neigh_a)
         {
             RTuple prev = find_prev(x, a, t);
@@ -340,8 +284,8 @@ public:
                 aux.insert({x, prev.Tminus});
             }
         }
-        // fazer neighbours out
 
+        // Vizinhos de saída
         for (auto x : neigh_b)
         {
             RTuple next = find_next(b, x, t + delta);
@@ -361,27 +305,35 @@ public:
                 }
             }
         }
-    };
+    }
+
     bool can_reach(string u_str, string v_str, int t1, int t2)
     {
+        // Método para verificar se é possível alcançar 'v' a partir de 'u' entre os tempos 't1' e 't2'
         int a = get_node_index(u_str);
         int b = get_node_index(v_str);
         set<RTuple, SetCompare> tuplas = hashTable[{a, b}];
         RTuple next = find_next(a, b, t1);
         RTuple prev = find_prev(a, b, t2);
-        if ((!next.empty() && next.Tplus <= t2) || !prev.empty() && prev.Tplus >= t1)
+        if ((!next.empty() && next.Tplus <= t2) || (!prev.empty() && prev.Tplus >= t1))
         {
             return true;
         }
         return false;
-    };
+    }
 
-    bool is_connected(int t1, int t2){};
+    bool is_connected(int t1, int t2)
+    {
+        // Método para verificar se o grafo é conectado entre os tempos 't1' e 't2'
+        // Implementação pendente
+        // Pode ser adicionada de acordo com os requisitos específicos
+    }
 
     void print_connections()
     {
+        // Método para imprimir as conexões no grafo
         for (int x = 0; x < tam; x++)
-        { // pair<pair<string,string>, set<Rtuple>>
+        {
             for (int y = 0; y < tam; y++)
             {
                 if (!hashTable[{x, y}].empty())
@@ -399,9 +351,10 @@ public:
     }
 };
 
+// Função principal
 int main(int argc, char const *argv[])
 {
- // Nome do arquivo a ser lido
+    // Nome do arquivo a ser lido
     std::string nomeArquivo = "teste.in";
 
     // Abre o arquivo
